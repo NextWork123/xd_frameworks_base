1da96a12e57bfd036e24c12250a875ec12940cfc
LuK1337 <priv.luk@gmail.com>
2018-10-31 16:31:19 +0100

    Squash of SystemUI: Show bluetooth battery level
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   2018-10-31 16:31:19 +0100
        SystemUI: Show bluetooth battery level when available
    
        * Somewhat inspired by change committed by Gavin Ni <gisngy@gmail.com>
          back in cm-13.0 days (see commit 88e7a6c). Since then completely
          rewritten using BluetoothDevice API introduced in Oreo, with new
          drawables meant to be used with 0-9 battery level range.
    
        Change-Id: I6179bfd41e033591534e8cf3c6adc98ce715a13d
    
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   2021-08-23 10:11:54 +0200
        fixup! SystemUI: Show bluetooth battery level when available
    
        The current update method is triggered in 2 cases:
          the SystemUI bluetooth controller changes device state
          the battery level changed intent is sent
    
        Sadly the current SysUI controller doesn't handle battery level
        changes, and is instead triggered in a few other cases ( ex audio
        state changed ).
        And since we use the data from SysUI controller for battery level
        from changes in the global bt controller we may have 2 different
        set of devices to work on. This could ( and does ) trigger
        race conditions.
    
        Instead add support battery level state changes within the SystemUI
        controller to work on the same data set.
    
        Change-Id: Ib57096fc2ebe151ac3860277a4db2319f230b514
    
    Change-Id: I34344907711b7b9fc4c1d15d4516bd53bf30ebb2

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -501,34 +501,26 @@ public class PhoneStatusBarPolicy
             if (mBluetooth.isBluetoothConnected()
                     && (mBluetooth.isBluetoothAudioActive()
                     || !mBluetooth.isBluetoothAudioProfileOnly())) {
-                List<CachedBluetoothDevice> connectedDevices = mBluetooth.getConnectedDevices();
-                int batteryLevel = connectedDevices.isEmpty() ? BATTERY_LEVEL_UNKNOWN
-                        : connectedDevices.stream()
-                                          .mapToInt(device -> device.getBatteryLevel())
-                                          .filter(level -> level != BATTERY_LEVEL_UNKNOWN)
-                                          .findFirst()
-                                          .orElse(BATTERY_LEVEL_UNKNOWN);
+                int batteryLevel = mBluetooth.getBatteryLevel();
                 if (batteryLevel == 100) {
-                    iconId = R.drawable.stat_sys_data_bluetooth_connected_battery_10;
-                } else if (batteryLevel >= 90) {
                     iconId = R.drawable.stat_sys_data_bluetooth_connected_battery_9;
-                } else if (batteryLevel >= 80) {
+                } else if (batteryLevel >= 90) {
                     iconId = R.drawable.stat_sys_data_bluetooth_connected_battery_8;
-                } else if (batteryLevel >= 70) {
+                } else if (batteryLevel >= 80) {
                     iconId = R.drawable.stat_sys_data_bluetooth_connected_battery_7;
-                } else if (batteryLevel >= 60) {
+                } else if (batteryLevel >= 70) {
                     iconId = R.drawable.stat_sys_data_bluetooth_connected_battery_6;
-                } else if (batteryLevel >= 50) {
+                } else if (batteryLevel >= 60) {
                     iconId = R.drawable.stat_sys_data_bluetooth_connected_battery_5;
-                } else if (batteryLevel >= 40) {
+                } else if (batteryLevel >= 50) {
                     iconId = R.drawable.stat_sys_data_bluetooth_connected_battery_4;
-                } else if (batteryLevel >= 30) {
+                } else if (batteryLevel >= 40) {
                     iconId = R.drawable.stat_sys_data_bluetooth_connected_battery_3;
-                } else if (batteryLevel >= 20) {
+                } else if (batteryLevel >= 30) {
                     iconId = R.drawable.stat_sys_data_bluetooth_connected_battery_2;
-                } else if (batteryLevel >= 10) {
+                } else if (batteryLevel >= 20) {
                     iconId = R.drawable.stat_sys_data_bluetooth_connected_battery_1;
-                } else if (batteryLevel >= 0) {
+                } else if (batteryLevel >= 10) {
                     iconId = R.drawable.stat_sys_data_bluetooth_connected_battery_0;
                 }
                 contentDescription = mResources.getString(
