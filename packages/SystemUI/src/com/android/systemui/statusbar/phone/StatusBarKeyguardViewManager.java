Project-Kaleidoscope/android_frameworks_base

SystemUI: HACK: Fix random wakeup when just sleeping
This hack fixes the problem that the device is randomly woke up just
after screen off to sleep, without changing the original behavior.

Still we need to wait for Google to come up with a real fix.

Change-Id: I5ce99e72bcffe160fca9f4478058d3fcb51eca78
Signed-off-by: LibXZR <i@xzr.moe>

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -74,6 +74,8 @@ import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
 
 import java.io.PrintWriter;
+import java.lang.StackTraceElement;
+import java.lang.Thread;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -123,9 +125,18 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
     private final BouncerExpansionCallback mExpansionCallback = new BouncerExpansionCallback() {
         @Override
         public void onFullyShown() {
+            boolean shouldWakeup = true;
+            for (StackTraceElement e : Thread.currentThread().getStackTrace()) {
+                if ("handleShow".equals(e.getMethodName())) {
+                    shouldWakeup = false;
+                    break;
+                }
+            }
             updateStates();
-            mStatusBar.wakeUpIfDozing(SystemClock.uptimeMillis(),
-                    mStatusBar.getBouncerContainer(), "BOUNCER_VISIBLE");
+            if (shouldWakeup) {
+                mStatusBar.wakeUpIfDozing(SystemClock.uptimeMillis(),
+                        mStatusBar.getBouncerContainer(), "BOUNCER_VISIBLE");
+            }
             onKeyguardBouncerFullyShownChanged(true);
             showFaceRecognizingMessage();
         }

NextWork123/xd_frameworks_base

Signed-off-by: Dmitrii <bankersenator@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -74,6 +74,8 @@ import com.android.systemui.unfold.FoldAodAnimationController;
 import com.android.systemui.unfold.SysUIUnfoldComponent;
 
 import java.io.PrintWriter;
+import java.lang.StackTraceElement;
+import java.lang.Thread;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Optional;
@@ -129,9 +131,18 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
         @Override
         public void onFullyShown() {
             mBouncerAnimating = false;
+            boolean shouldWakeup = true;
+            for (StackTraceElement e : Thread.currentThread().getStackTrace()) {
+                if ("handleShow".equals(e.getMethodName())) {
+                    shouldWakeup = false;
+                    break;
+                }
+            }
             updateStates();
-            mCentralSurfaces.wakeUpIfDozing(SystemClock.uptimeMillis(),
-                    mCentralSurfaces.getBouncerContainer(), "BOUNCER_VISIBLE");
+            if (shouldWakeup) {
+                mCentralSurfaces.wakeUpIfDozing(SystemClock.uptimeMillis(),
+                        mCentralSurfaces.getBouncerContainer(), "BOUNCER_VISIBLE");
+            }
         }
 
         @Override